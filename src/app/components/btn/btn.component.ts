d2581116d2a4e310cd590f2cb6f75434bbfde6ea
nicolasviruel <nicolasviruel@gmail.com>
2024-04-11 01:32:09 -0300

    se agrega el proyecto

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-btn',
+  templateUrl: './btn.component.html',
+})
+export class BtnComponent implements OnInit {
+
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
