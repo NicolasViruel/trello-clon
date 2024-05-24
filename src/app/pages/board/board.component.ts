f895e190e0da0032a42a00112333d964aaf3d78f
nicolasviruel <nicolasviruel@gmail.com>
2024-05-23 23:53:33 -0300

    Se realiza la segunda parte del DragDrop en la seccion de board

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop'; 
+import { ToDo } from 'src/app/models/todo.model';
 
 @Component({
   selector: 'app-board',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  todos: ToDo[]=[
+    {
+      id: '1',
+      title: 'Task 1',
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+    },
+    {
+      id: '3',
+      title: 'Task 3',
+    }
+  ];
+
+  doing: ToDo[] = [];
+  done: ToDo[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    //aca hacemos que se guarde en nuestro elemento array con la nueva posicion que va a tener
+    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
   }
 
 }
