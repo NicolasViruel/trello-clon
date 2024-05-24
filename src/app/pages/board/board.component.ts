4d999f17111d5b07e0251c08df3576b7627bed05
nicolasviruel <nicolasviruel@gmail.com>
2024-05-24 01:10:23 -0300

    Se comienza a trabajar en el dinamismo del DragAndDrop entre las columnas, ya que la tenemos de forma hardcode

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop'; 
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'; 
 import { ToDo } from 'src/app/models/todo.model';
 
 @Component({
@@ -12,29 +12,47 @@ export class BoardComponent implements OnInit {
   todos: ToDo[]=[
     {
       id: '1',
-      title: 'Task 1',
+      title: 'Make dishes',
     },
     {
       id: '2',
-      title: 'Task 2',
+      title: 'Buy a unicorn',
     },
-    {
-      id: '3',
-      title: 'Task 3',
-    }
   ];
 
-  doing: ToDo[] = [];
-  done: ToDo[] = [];
+  doing: ToDo[] = [
+    {
+      id: '3',
+      title: 'Watch Angular Path in visual',
+    }
+  ];
+  done: ToDo[] = [
+    {
+      id: '4',
+      title: 'Play video games',
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  drop(event: CdkDragDrop<any[]>) {
-    //aca hacemos que se guarde en nuestro elemento array con la nueva posicion que va a tener
-    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+  drop(event: CdkDragDrop<ToDo[]>) {
+    // ahora necesito saber a cual columna reordenar la tarea que estoy moviendo para eso se utiliza el "Transfer"
+    if (event.previousContainer === event.container) {
+      //aca hacemos que se guarde en nuestro elemento array con la nueva posicion que va a tener
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else{
+      //ahora si estoy moviendo hacia otra columna debo utilizar el transfer
+      transferArrayItem(
+        event.previousContainer.data, //primero envio donde estaba la tarea
+        event.container.data, //a donde va la nueva tarea
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
   }
 
 }
